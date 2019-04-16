977cc6915548aa6d1e7267a8755453ccfae4e36c
LucasParsaGiraldi <49210267+LucasParsaGiraldi@users.noreply.github.com>
2019-04-16 14:46:32 -0300

    Classes Funcionario,Gerente,Pessoa

---
+++ ToPunch/src/topunch/Gerente.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package topunch;
+
+/**
+ *
+ * @author Senai
+ */
+public class Gerente extends Pessoa {
+    
+    
+    
+     public Gerente (String nome,String endereco,String telefone,int id){
+        super(nome,endereco,telefone,id);
+     }
+     
+     
+     public boolean inserirProduto (Produto p){
+         
+     }
+     public boolean alterarProduto (Produto pNovo,Produto p){
+         
+     }
+     public void excluirProduto (Produto p){
+         
+     }
+     public void excluirCliente (Cliente c){
+         
+     }
+     
+     
+     
+     
+     
+     
+}
--- ToPunch/src/topunch/Pessoa.java
+++ ToPunch/src/topunch/Pessoa.java
@@ -10,5 +10,51 @@ package topunch;
  * @author Senai
  */
 public class Pessoa {
+    private String  nome;
+    private String endereco;
+    private String telefone;
+    private int id;
+
+    public Pessoa(String nome, String endereco, String telefone, int id) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+    
     
 }
